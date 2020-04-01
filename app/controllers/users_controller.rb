afb1078ceb8a4af54603e7495d3d8b82d124d305
knhs <knhskunibo@gmail.com>
2020-04-01 18:14:47 +0900

    resources使わない

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(uesr_params)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(uesr_params)
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.delete
+    end
+
+    private
+
+    def uesr_params
+        params.require(:user).permit(:name, :age)
+    end
+end
